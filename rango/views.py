3ab7bd9a71c74a66c39578283f949d98926c01c3
Olivia Foulds <okfoulds@gmail.com>
2019-08-13 16:24:43 +0100

    End of chapter 4

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
 def index(request):
-    return HttpResponse("Rango says hey there partner! Olivia will be a geek soon")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
 
 
 # Create your views here.
