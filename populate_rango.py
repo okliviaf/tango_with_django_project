4f590754bc9f6e3db40a587afb4b0ff51ae6e5fa
Olivia Foulds <okfoulds@gmail.com>
2019-09-01 21:37:01 +0100

    corrected a mistake

--- populate_rango.py
+++ populate_rango.py
@@ -41,7 +41,7 @@ def populate():
         'views': 97}
     ]
 
-    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
             'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
             'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16},
